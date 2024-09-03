ba6c2e257e44b13bc9110d0a1c8ea986b5b87b43
xan-develop <x.develop@outlook.es>
2024-09-03 19:16:03 +0100

    todo

---
+++ src/reports/reports.entity.ts
@@ -0,0 +1,11 @@
+import { Entity , PrimaryGeneratedColumn , Column ,  } from "typeorm";
+
+@Entity()
+export class Report {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    price: number;
+}
